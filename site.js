c69b4bdf246dc608027529004cc62aa689267004
yoav niran <yoavniran@gmail.com>
2015-06-14 12:44:08 +0300

    adding site.js

---
+++ site.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+    
+    function init() {
+        doIntoAnimation();
+        registerEvents();
+    }
+
+    function registerEvents() {
+
+        $(".main-menu-item").click(onMenuItemClick);
+
+    }
+
+    function doIntoAnimation() {
+        setTimeout(function () {
+
+            var bgImgFilter = "blur(6px) contrast(130%)";
+            $("#bgimg").css({ "-webkit-filter": bgImgFilter, "filter": bgImgFilter });
+            $("footer").css("opacity", "1");
+
+        }, 200);
+    }
+
+    function onMenuItemClick() {
+
+        console.log(this);
+
+
+    }
+
+
+    init();
+});
