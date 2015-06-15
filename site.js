932e7ec6224c85345ab0d0499ea5741326940ab1
yoav niran <yoavniran@gmail.com>
2015-06-15 19:30:10 +0300

    mobile menu working, logo returns to beginning

--- site.js
+++ site.js
@@ -1,35 +1,132 @@
-﻿$(function () {
-    
+﻿$("document").ready(function () {
+
+    var siteContent = {
+            about: {
+                title: "אודותנו",
+                textElement: "#about-content"
+            }
+        },
+
+        ui = {},
+        selectedContent = null,
+        mobileMenuItemsCopied = false;
+
     function init() {
-        doIntoAnimation();
+        doIntroAnimation();
+        initUi();
+        copyMobileMenuItems();
         registerEvents();
+    }
+
+    function copyMobileMenuItems() {
+
+        if (!mobileMenuItemsCopied) {
+            var items = ui.menuItems.clone();
+
+            items.addClass("mobile");
+            items.appendTo(ui.mobileMenuItems);
+
+            ui.menuItems = ui.menuItems.add(items);
+
+            mobileMenuItemsCopied = true;
+        }
+    }
+
+    function initUi() {
+        ui.menuItems = $(".main-menu-item");
+        ui.mobileMenuItems = $("#mobile-menu-items");
+        ui.mobileMenuContainer = $("#mobile-menu-container");
+        ui.contentTitle = $("#page-content-container h2");
+        ui.homeContent = $("#home-content-container");
+        ui.mobileMenu = $("#mobile-menu");
     }
 
     function registerEvents() {
 
-        $(".main-menu-item").click(onMenuItemClick);
-        $("#mobile-menu").click(onMobileMenuClick);
+        ui.menuItems.click(onMenuItemClick);
+        ui.mobileMenu.click(onMobileMenuClick);
+
+        $("#site-logo").click(onLogoClick);
+        $("body").click(onBodyClick);
     }
 
-    function doIntoAnimation() {
+    function doIntroAnimation() {
         setTimeout(function () {
 
             var bgImgFilter = "blur(6px) contrast(60%) drop-shadow(16px 16px 10px rgb(159, 92, 231)) ";
-            $("#bgimg").css({ "-webkit-filter": bgImgFilter, "filter": bgImgFilter });
-            $(".intro-delayed").css("opacity", "1");
+            $("#bgimg").css({"-webkit-filter": bgImgFilter, "filter": bgImgFilter});
+
+            toggleDelayedElement(".intro-delayed", true);
 
         }, 200);
+    }  
+
+    function toggleDelayedElement(selector, show, quickHide){
+        selector = (selector instanceof jQuery) ? selector : $(selector);
+
+        if (quickHide) {
+        	selector.hide();
+        	setTimeout(function () {
+        		selector.show();
+        	}, 1);
+        }
+		
+		selector.toggleClass("delayed-visible", show);
+    }
+
+    function onBodyClick(e) {
+        var target = e.target || e.srcElement;
+
+        if (target !== ui.mobileMenu[0]) {
+            ui.mobileMenuContainer.toggleClass("mobile-menu-open", false);
+        }
     }
 
     function onMenuItemClick() {
+      
+        var contentId = this.dataset.menuItem;
 
-        console.log(this);
+        setActiveMenuItem(contentId);
+        showPageContent(contentId);
+    }
 
+    function setActiveMenuItem(contentId) {
 
+        ui.menuItems.each(function (idx, item) {
+
+            if (item.dataset.menuItem === contentId) {
+                item.classList.add("active");
+            }
+            else {
+                item.classList.remove("active");
+            }
+        });
+    }
+
+    function showPageContent(contentId) {
+        var contentItem = siteContent[contentId];
+
+        if (contentItem) {
+            toggleDelayedElement(ui.homeContent, false);
+            toggleDelayedElement("#page-container", true);
+
+            ui.contentTitle.text(contentItem.title);
+            selectedContent = $(contentItem.textElement);
+            toggleDelayedElement(selectedContent, true);
+        }
+        else {
+            console.error("couldnt find content item: " + contentId);
+        }
     }
 
     function onMobileMenuClick() {
-        console.log("mobile click!");
+        ui.mobileMenuContainer.toggleClass("mobile-menu-open");
+    }
+
+    function onLogoClick() {
+        toggleDelayedElement(ui.homeContent, true);
+        toggleDelayedElement("#page-container", false, true);
+        setActiveMenuItem(null);
     }
 
     init();
